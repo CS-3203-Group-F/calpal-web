5dd55210fbd22dfdb7264315fdb2f23f67675bae
EvanWiggs <evanwiggs1@gmail.com>
2024-03-29 15:27:53 -0500

    Added VerifyCode Page
    //Not finished

--- app/ResetPassword/VerifyCode/page.tsx
+++ app/ResetPassword/VerifyCode/page.tsx
@@ -1,11 +1,11 @@
 "use client"
 import React, {useState} from "react";
-import styles from '../Login/Login.module.css';
+import styles from '../../Login/Login.module.css';
 import Link from 'next/link';
 
 
 export const passReset =()=> {
-    const[email, setEmail] = useState('');
+    const[code, setCode] = useState('');
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>)=> {
         e.preventDefault();
@@ -17,25 +17,25 @@ export const passReset =()=> {
         <div className={styles.loginBox}>
         <div className={styles.logoContainer}>
           {/* Logo or title; replace with actual logo image */}
-          <h1 className={styles.title} style={{ height: '100%', width: '100%', textAlign: "center"}}>Reset Password</h1>
-          <p style ={{textAlign: "center"}}>Enter the email associated with your account and we’ll send an email with a code to reset your password.</p>
+          <h1 className={styles.title} style={{ height: '100%', width: '100%', textAlign: "center"}}>Check Your Mail</h1>
+          <p style ={{textAlign: "center"}}>We have sent a code to your email to reset your password. Enter the code below</p>
           </div>
 
             <form onSubmit={handleSubmit} className={styles.loginForm}>
                 {/*Input Fields of login Page */}
                 <input
                     className={styles.input}
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    type="email"
-                    placeholder="Email"
-                    id="email"
-                    name="email"
+                    value={code}
+                    onChange={(e) => setCode(e.target.value)}
+                    type="number"
+                    placeholder="Enter Code"
+                    id="number"
+                    name="number"
                 />
                 
                 <button type="submit" className={styles.loginButton}>Next</button>
                 
-                <Link href="../Login" passHref>
+                <Link href="../ResetPassword" passHref>
             <button  className={styles.signUpButton} style = {{marginTop: '10px'}}>Go Back</button>
             </Link>
             </form>
