b5ed5ce75911aa42c1d7be2c3d90ee684db9df15
EvanWiggs <evanwiggs1@gmail.com>
2024-04-08 21:13:27 -0500

    more progress

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 "use client"
 import { useState } from "react";
-import './Settings.css'; // Import your CSS
+import './Settings.css'; 
 
 export const Settings = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [currentView, setCurrentView] = useState('view1'); //allows you to switch between different views of the mainBox
+  const [selectedButton, setSelectedButton] = useState('view1'); // allows you to change the color of the selected button
+  const accountName = 'Lauren Dodge'; //hardcoded account name
 
   const openSettings = () => {
     setIsOpen(true);
@@ -13,24 +16,35 @@ export const Settings = () => {
     setIsOpen(false);
   };
 
+  const handleButtonClick = (view:string) => { //function to handle button click
+    setCurrentView(view);
+    setSelectedButton(view);
+  };
+
   return (
     <div>
       <button onClick={openSettings}>Settings</button>
 
       {isOpen && (
-        <div className="backdrop" onClick={closeSettings}>
+        <div className="backdrop">
           <div className="settingsContainer">
         
            
             <div className="leftPanel">
               <h1 className ="title" style={{ paddingTop: '50px', paddingLeft: '20px' }}>Settings</h1>
-                  {/* Add your settings here */}
+              <h2 className="subtitle" style={{ paddingLeft: '20px' }}>{accountName}</h2>
+              <button className={ `leftPanelButton  ${selectedButton === 'view1' ? 'selected' : ''}`} onClick={() => handleButtonClick('view1')}>Public Profile</button> 
+              <button className={ `leftPanelButton  ${selectedButton === 'view2' ? 'selected' : ''}`} onClick={() => handleButtonClick('view2')}>Account</button> 
+              <button className={ `leftPanelButton  ${selectedButton === 'view3' ? 'selected' : ''}`} onClick={() => handleButtonClick('view3')}>Appearance</button> 
+                  {/* Add  settings here */}
                   <button onClick={closeSettings}>Close</button>
             
             </div>
 
             <div className="mainBox">
-              
+              {currentView === 'view1' && <div>Content for View 1</div>} 
+              {currentView === 'view2' && <div>Content for View 2</div>}
+              {currentView === 'view3' && <div>Content for View 3</div>}
               
             </div>
 
