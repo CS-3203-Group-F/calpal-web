6be06f67fd1794f5bbf257bbc41365e71d5d772d
mrboom192 <mrboom192@gmail.com>
2024-04-22 01:49:55 -0500

    started on event form

--- app/components/Calendar/CalendarHeader.tsx
+++ app/components/Calendar/CalendarHeader.tsx
@@ -1,9 +1,14 @@
-import { useState, useRef } from "react";
+"use client";
+
+import { useState, useRef, ReactNode } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
 import {
   CalendarMonth,
   CalendarViewMonth,
   CalendarViewWeek,
   Add,
+  CalendarAddOn,
+  Close,
 } from "../Icons";
 import FullCalendar from "@fullcalendar/react";
 
@@ -36,10 +41,12 @@ const formattedDate = month + " " + day + ", " + year;
 
 export function CalendarHeader(props: {
   calendarRef: React.RefObject<FullCalendar>;
+  events: any;
+  setEvents: any;
 }) {
   const [currentView, setCurrentView] = useState("dayGridMonth");
 
-  function changeView() {
+  function handleChangeView() {
     const calendarApi = props.calendarRef.current!.getApi();
     const newView =
       currentView === "dayGridMonth" ? "timeGridWeek" : "dayGridMonth";
@@ -47,11 +54,16 @@ export function CalendarHeader(props: {
     calendarApi.changeView(newView);
   }
 
+  function handleJumpToToday() {
+    const calendarApi = props.calendarRef.current!.getApi();
+    calendarApi.today();
+  }
+
   return (
     <div className="flex flex-row w-full h-max justify-between items-center p-6">
       <div className="flex flex-row items-center gap-4">
         <button
-          onClick={changeView}
+          onClick={handleChangeView}
           className="flex px-3 py-2 items-center rounded-lg bg-amber-400 hover:bg-amber-500"
         >
           {currentView === "dayGridMonth" ? (
@@ -60,17 +72,23 @@ export function CalendarHeader(props: {
             <CalendarViewMonth />
           )}
         </button>
-        <span className="font-semibold text-xl">{formattedDate}</span>
+        <button onClick={handleJumpToToday}>
+          <span className="font-semibold text-xl">{formattedDate}</span>
+        </button>
       </div>
       <div className="flex flex-row gap-2">
         <FilterButton category="All" color="#000" />
         <FilterButton category="School" color="#F886A8" />
         <FilterButton category="Work" color="#DFDD6C" />
-        <AddEventButton />
+        <AddEventButton>
+          <AddEventForm />
+        </AddEventButton>
       </div>
     </div>
   );
 }
+
+// NEED TO ADD IN WEEK NAVIGATION (CHEVRON BUTTONS)
 
 function FilterButton(props: { category: string; color: string }) {
   return (
@@ -83,11 +101,104 @@ function FilterButton(props: { category: string; color: string }) {
   );
 }
 
-function AddEventButton() {
+function AddEventButton(props: { children: any }) {
+  const [open, setOpen] = useState(false);
+
   return (
-    <button className="flex flex-row items-center px-3 py-2 gap-2 rounded-lg bg-amber-400 hover:bg-amber-500">
-      <Add />
-      <span className="font-bold text-base">New event</span>
-    </button>
+    <div className="relative">
+      <button
+        onClick={() => setOpen((curr) => !curr)}
+        className="flex flex-row items-center px-3 py-2 gap-2 rounded-lg bg-amber-400 hover:bg-amber-500"
+      >
+        <Add />
+        <span className="font-bold text-base">New event</span>
+      </button>
+      {open && props.children}
+    </div>
   );
+}
+
+type FormData = {
+  title: string;
+  location: string;
+  description: string;
+  allDay: boolean;
+  start: string;
+  end: string;
+  notes: string;
+};
+
+function AddEventForm() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<FormData>();
+
+  // Prints form data
+  const onSubmit: SubmitHandler<FormData> = (data) => console.log(data);
+
+  return (
+    <div
+      style={{ transform: "translateX(-45%)" }}
+      className="flex flex-col items-center rounded-lg overflow-hidden absolute top-[58px] w-72 h-max bg-white shadow-lg z-50"
+    >
+      <AddEventFormHeader />
+      <div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input
+            placeholder="Title"
+            {...register("title", { required: true })}
+          />
+
+          <input placeholder="Location (optional)" {...register("location")} />
+
+          <input
+            placeholder="Description (optional)"
+            {...register("description")}
+          />
+
+          <label htmlFor="allDay">All-day?</label>
+          <input id="allDay" type="checkbox" {...register("allDay")} />
+
+          <label htmlFor="start">Choose a time for your appointment:</label>
+          <input
+            type="datetime-local"
+            id="start"
+            {...(register("start"), { required: true })}
+          />
+
+          <label htmlFor="end">Choose a time for your appointment:</label>
+          <input type="datetime-local" id="end" {...register("end")} />
+
+          <input placeholder="Notes (optional)" {...register("notes")} />
+
+          <input type="submit" />
+        </form>
+      </div>
+    </div>
+  );
+}
+
+function AddEventFormInputs() {}
+
+function AddEventFormHeader() {
+  return (
+    <div className="flex p-4 justify-between items-center self-stretch bg-stone-100">
+      <div className="flex flex-row items-center gap-2">
+        <CalendarAddOn />
+        <p className="text-base font-bold">New event</p>
+      </div>
+      <button onClick={() => console.log("NEED TO ADD CLOSING FUNCTIONALITY")}>
+        <Close />
+      </button>
+    </div>
+  );
+}
+
+{
+  /* <div className="flex flex-col gap-2">
+
+</div> */
 }
--- app/components/Calendar/CalendarPanel.tsx
+++ app/components/Calendar/CalendarPanel.tsx
@@ -14,95 +14,98 @@ import {
 } from "@fullcalendar/core/index.js";
 import { CalendarContentProps } from "@fullcalendar/core/internal";
 
-const groupFDataExample = {
-  events: [
-    {
-      title: "CalPal planning",
-      description: "Planning session for the upcoming CalPal event.",
-      location: "Devon Energy Hall 0270",
-      start: "2024-04-17T10:00:00.000Z",
-      end: "2024-04-17T12:00:00.000Z",
-      editable: true,
-      classNames: [
-        "bg-yellow-200/50 border rounded-lg font-semibold border-yellow-500",
-      ],
-      textColor: "#713F12",
-    },
-    {
-      title: "CalPal planning",
-      description: "Planning session for the upcoming CalPal event.",
-      location: "Devon Energy Hall 0270",
-      start: "2024-04-17T14:00:00.000Z",
-      allDay: true,
-      editable: true,
-      classNames: [
-        "bg-cyan-200/50 border rounded-lg font-semibold border-cyan-500",
-      ],
-      textColor: "#06B6D4",
-    },
-    {
-      title: "Earnings Report",
-      description: "Review and discussion of quarterly earnings report.",
-      location: "CalPal Business Park Building 3, Room 2100",
-      start: "2024-04-17T09:00:00.000Z",
-      end: "2024-04-17T11:00:00.000Z",
-      editable: true,
-      classNames: [
-        "bg-pink-200/50 border rounded-lg font-semibold border-pink-500",
-      ],
-      textColor: "#831843",
-    },
-    {
-      title: "Bonding trip",
-      description: "Group bonding trip for members of Group F.",
-      date: "2024-04-18T09:00:00",
-      location: "Phoenix, Arizona",
-      start: "2024-04-18T09:00:00.000Z",
-      end: "2024-04-18T17:00:00.000Z",
-      editable: true,
-      classNames: [
-        "bg-red-200/50 border rounded-lg font-semibold border-red-500",
-      ],
-      textColor: "#EF4444",
-    },
-    {
-      title: "New Event 1",
-      description: "Description of new event 1.",
-      location: "Location of new event 1",
-      start: "2024-04-17T15:00:00.000Z",
-      end: "2024-04-17T16:00:00.000Z",
-      editable: true,
-      classNames: [
-        "bg-blue-200/50 border rounded-lg font-semibold border-blue-500",
-      ],
-      textColor: "#3B82F6",
-    },
-    {
-      title: "New Event 2",
-      description: "Description of new event 2.",
-      location: "Location of new event 2",
-      start: "2024-04-18T13:00:00.000Z",
-      end: "2024-04-18T14:00:00.000Z",
-      editable: true,
-      classNames: [
-        "bg-green-200/50 border rounded-lg font-semibold border-green-500",
-      ],
-      textColor: "#10B981",
-    },
-  ],
-};
+const groupFDataExample = [
+  {
+    title: "CalPal planning",
+    description: "Planning session for the upcoming CalPal event.",
+    location: "Devon Energy Hall 0270",
+    start: "2024-04-17T10:00:00.000Z",
+    end: "2024-04-17T12:00:00.000Z",
+    editable: true,
+    classNames: [
+      "bg-yellow-200/50 border rounded-lg font-semibold border-yellow-500",
+    ],
+    textColor: "#713F12",
+  },
+  {
+    title: "CalPal planning",
+    description: "Planning session for the upcoming CalPal event.",
+    location: "Devon Energy Hall 0270",
+    start: "2024-04-17T14:00:00.000Z",
+    allDay: true,
+    editable: true,
+    classNames: [
+      "bg-cyan-200/50 border rounded-lg font-semibold border-cyan-500",
+    ],
+    textColor: "#06B6D4",
+  },
+  {
+    title: "Earnings Report",
+    description: "Review and discussion of quarterly earnings report.",
+    location: "CalPal Business Park Building 3, Room 2100",
+    start: "2024-04-17T09:00:00.000Z",
+    end: "2024-04-17T11:00:00.000Z",
+    editable: true,
+    classNames: [
+      "bg-pink-200/50 border rounded-lg font-semibold border-pink-500",
+    ],
+    textColor: "#831843",
+  },
+  {
+    title: "Bonding trip",
+    description: "Group bonding trip for members of Group F.",
+    date: "2024-04-18T09:00:00",
+    location: "Phoenix, Arizona",
+    start: "2024-04-18T09:00:00.000Z",
+    end: "2024-04-18T17:00:00.000Z",
+    editable: true,
+    classNames: [
+      "bg-red-200/50 border rounded-lg font-semibold border-red-500",
+    ],
+    textColor: "#EF4444",
+  },
+  {
+    title: "New Event 1",
+    description: "Description of new event 1.",
+    location: "Location of new event 1",
+    start: "2024-04-17T15:00:00.000Z",
+    end: "2024-04-17T16:00:00.000Z",
+    editable: true,
+    classNames: [
+      "bg-blue-200/50 border rounded-lg font-semibold border-blue-500",
+    ],
+    textColor: "#3B82F6",
+  },
+  {
+    title: "New Event 2",
+    description: "Description of new event 2.",
+    location: "Location of new event 2",
+    start: "2024-04-18T13:00:00.000Z",
+    end: "2024-04-18T14:00:00.000Z",
+    editable: true,
+    classNames: [
+      "bg-green-200/50 border rounded-lg font-semibold border-green-500",
+    ],
+    textColor: "#10B981",
+  },
+];
 
 export default function CalendarPanel() {
+  const [events, setEvents] = useState(groupFDataExample);
   const calendarRef = useRef<FullCalendar>(null);
 
   return (
     <div className="flex flex-col w-screen h-screen overflow-auto">
-      <CalendarHeader calendarRef={calendarRef} />
+      <CalendarHeader
+        calendarRef={calendarRef}
+        events={events}
+        setEvents={setEvents}
+      />
       <FullCalendar
         plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
         ref={calendarRef}
         initialView="dayGridMonth"
-        events={groupFDataExample}
+        events={events}
         eventClassNames={"border rounded font-semibold py-2 px-1 shadow"}
         height={"100%"}
         headerToolbar={false}
