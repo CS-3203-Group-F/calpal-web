4bc9f37fe6590f03d05324af64de071f2374dcc3
Jason <mrboom192@gmail.com>
2024-04-30 19:35:39 -0500

    Fixed layouts

--- app/Groups/page.tsx
+++
@@ -1,6 +0,0 @@
-import { Group } from "../components/Group/Group";
-import GroupsLoading from "./loading";
-
-export default function Groups() {
-  return <Group />;
-}
--- app/page.tsx
+++ app/page.tsx
@@ -1,56 +1,20 @@
-import { Group } from "./components/Group/Group";
-import { GroupCard } from "./components/Panel/GroupCard";
-import { Panel } from "./components/Panel/Panel";
-import { Rsvp } from "./components/Panel/Rsvp";
-import { Section } from "./components/Panel/Section";
-import { ToolboxCard } from "./components/Panel/Toolbox";
-import Settings from "./components/Settings/Settings";
+import Link from "next/link";
 
-export default function Home() {
+export default function Calendar() {
   return (
     <div className="flex flex-col items-center self-stretch p-6 gap-8">
-      <Rsvp />
-      <Section title="Groups">
-        <div className="flex flex-col gap-2">
-          <GroupCard
-            color="bg-orange-300"
-            name="Linear Algebra"
-            members="14"
-            route="/Groups"
-          ></GroupCard>
-          <GroupCard
-            color="bg-lime-500"
-            name="comp org"
-            members="3"
-            route=""
-          ></GroupCard>
-          <GroupCard
-            color="bg-red-500"
-            name="roommates"
-            members="4"
-            route=""
-          ></GroupCard>
-        </div>
-      </Section>
-      <Section title="Toolbox">
-        <div className="flex flex-col gap-4">
-          <ToolboxCard
-            imgURL="toolboxPictures\toolbox-groups.png"
-            name="Find groups"
-            description="Mingle with other pals"
-          ></ToolboxCard>
-          <ToolboxCard
-            imgURL="toolboxPictures\toolbox-import.png"
-            name="Plan a meeting"
-            description="with CalPal"
-          ></ToolboxCard>
-          <ToolboxCard
-            imgURL="toolboxPictures\toolbox-schedule.png"
-            name="Import schedules"
-            description="into CalPal"
-          ></ToolboxCard>
-        </div>
-      </Section>
+      <Link
+        href="/Calendar"
+        className="flex flex-row items-center justify-between p-4 bg-stone-50 rounded-lg hover:bg-stone-200 cursor-pointer"
+      >
+        Calendar
+      </Link>
+      <Link
+        href="/Airlock"
+        className="flex flex-row items-center justify-between p-4 bg-stone-50 rounded-lg hover:bg-stone-200 cursor-pointer"
+      >
+        Landing Page
+      </Link>
     </div>
   );
 }
