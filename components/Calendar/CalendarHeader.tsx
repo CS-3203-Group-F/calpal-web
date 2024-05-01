3b11bd193fd5c1a342264fc669ffcbee3bb24821
JasonNguyen2004 <mrboom192@gmail.com>
2024-05-01 01:31:05 -0500

    Added week and month arrow navigation

--- components/Calendar/CalendarHeader.tsx
+++ components/Calendar/CalendarHeader.tsx
@@ -10,6 +10,8 @@ import {
   Add,
   CalendarAddOn,
   Close,
+  ChevronLeft,
+  ChevronRight,
 } from "../Icons";
 import FullCalendar from "@fullcalendar/react";
 import Settings from "../Settings/Settings";
@@ -66,6 +68,16 @@ export function CalendarHeader(props: {
     calendarApi.today();
   }
 
+  function handleJumpNext() {
+    const calendarApi = props.calendarRef.current!.getApi();
+    calendarApi.next();
+  }
+
+  function handleJumpPrev() {
+    const calendarApi = props.calendarRef.current!.getApi();
+    calendarApi.prev();
+  }
+
   return (
     <div className="flex flex-row w-full h-max justify-between items-center p-6">
       <div className="flex flex-row items-center gap-4">
@@ -79,10 +91,22 @@ export function CalendarHeader(props: {
             <CalendarViewMonth />
           )}
         </button>
+        <button
+          onClick={handleJumpPrev}
+          className="hover:-translate-x-0.5 transition-transform"
+        >
+          <ChevronLeft />
+        </button>
         <button onClick={handleJumpToToday}>
           <span className="font-semibold text-xl" suppressHydrationWarning>
             {formattedDate}
           </span>
+        </button>
+        <button
+          onClick={handleJumpNext}
+          className="hover:translate-x-0.5 transition-transform"
+        >
+          <ChevronRight />
         </button>
       </div>
       <div className="flex flex-row gap-2">
