fdbf508e64243a86c867712885f86e2a793ee9ff
Jason <mrboom192@gmail.com>
2024-04-30 19:16:37 -0500

    update

--- app/Login/page.tsx
+++ app/Login/page.tsx
@@ -1,99 +1,97 @@
 "use client";
-import styles from './Login.module.css';
-import React, { useState}  from "react";
-import Link from 'next/link';
+import styles from "./Login.module.css";
+import React, { useState } from "react";
+import Link from "next/link";
 
+export const Login = () => {
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
 
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
-  
-  export const Login =()=> { 
-    const [email, setEmail] = useState('');
-    const [pass, setPass] = useState('');
+    const makeRequest = async () => {
+      try {
+        const res = await fetch("http://35.233.194.137/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+          body: `username=${encodeURIComponent(
+            email
+          )}&password=${encodeURIComponent(pass)}`,
+        });
+        console.log(res);
 
+        if (res.status === 200) {
+          // Redirect to home page or any other page after successful login
+          window.location.href = "/"; // Redirect to home page
+          console.log("Login successful");
+        } else {
+          // Handle failed login
+          console.error("Login failed");
+        }
+      } catch (error) {
+        console.error("An error occurred while logging in:", error);
+      }
+    };
 
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>)=> {
-        e.preventDefault();
+    makeRequest();
 
-
-        const makeRequest = async () => {
-            try {
-                const res = await fetch('http://35.233.194.137/login', {
-                    method: 'POST',
-                    headers: {
-                      'Content-Type': 'application/x-www-form-urlencoded',
-                    },
-                    body: `username=${encodeURIComponent(email)}&password=${encodeURIComponent(pass)}`
-                  });
-                console.log(res);
-                
-        
-                if (res.status === 200) {
-                    // Redirect to home page or any other page after successful login
-                   window.location.href = '/'; // Redirect to home page
-                   console.log('Login successful');
-                } else {
-                    // Handle failed login
-                    console.error('Login failed');
-                }
-            } catch (error) {
-                console.error('An error occurred while logging in:', error);
-            }
-        };
-
-        makeRequest();
-        
-        //submit form logic goes here
-
-    }
-    return (
-        <div className={styles.screenContainer}>
-        <div className={styles.loginBox}>
+    //submit form logic goes here
+  };
+  return (
+    <div className={styles.screenContainer}>
+      <div className={styles.loginBox}>
         <div className={styles.logoContainer}>
-        {/*<img src="/Images/logo.png" alt="Logo"/>*/}
+          {/*<img src="/Images/logo.png" alt="Logo"/>*/}
           <h1 className={styles.title}>CalPal</h1>
-          </div>
-
-            <form onSubmit={handleSubmit} className={styles.loginForm}>
-                {/*Input Fields of login Page */}
-                <input
-                    className={styles.input}
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    type="email"
-                    placeholder="Email or Username"
-                    id="email"
-                    name="email"
-                />
-                
-                <input
-                    className={styles.input}
-                    value={pass}
-                    onChange={(e) => setPass(e.target.value)}
-                    type="password"
-                    placeholder="Password"
-                    id="password"
-                    name="password"
-                />
-                <div className={styles.forgotPassword}>
-                    {/* Forgot Password link */}
-                    <a href="../ResetPassword" className={styles.forgotPasswordLink}>Forgot Password?</a>
-                </div>
-                <button type="submit" className={styles.loginButton}>Login</button>
-            </form>
-
-            <div className={styles.orSeparator}>
-                <div className={styles.line}></div>
-                <span>or</span>
-                <div className={styles.line}></div>
-            </div>
-            
-            {/*Link is being passed as a button to Register page*/}
-            <Link href="../Register" passHref>
-            <button  className={styles.signUpButton}>Sign Up</button>
-            </Link>
         </div>
-    </div>
-);
 
+        <form onSubmit={handleSubmit} className={styles.loginForm}>
+          {/*Input Fields of login Page */}
+          <input
+            className={styles.input}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            type="email"
+            placeholder="Email or Username"
+            id="email"
+            name="email"
+          />
+
+          <input
+            className={styles.input}
+            value={pass}
+            onChange={(e) => setPass(e.target.value)}
+            type="password"
+            placeholder="Password"
+            id="password"
+            name="password"
+          />
+          <div className={styles.forgotPassword}>
+            {/* Forgot Password link */}
+            <a href="../ResetPassword" className={styles.forgotPasswordLink}>
+              Forgot Password?
+            </a>
+          </div>
+          <button type="submit" className={styles.loginButton}>
+            Login
+          </button>
+        </form>
+
+        <div className={styles.orSeparator}>
+          <div className={styles.line}></div>
+          <span>or</span>
+          <div className={styles.line}></div>
+        </div>
+
+        {/*Link is being passed as a button to Register page*/}
+        <Link href="../Register" passHref>
+          <button className={styles.signUpButton}>Sign Up</button>
+        </Link>
+      </div>
+    </div>
+  );
 };
-export default Login; 
+export default Login;
---
+++ app/lib/session.ts
@@ -0,0 +1,41 @@
+import "server-only";
+import { cookies } from "next/headers";
+import { SignJWT, jwtVerify } from "jose";
+import { SessionPayload } from "@/app/lib/definitions";
+
+// Encrypting and decrypting sessions
+const secretKey = process.env.SESSION_SECRET;
+const encodedKey = new TextEncoder().encode(secretKey);
+
+export async function encrypt(payload: SessionPayload) {
+  return new SignJWT(payload)
+    .setProtectedHeader({ alg: "HS256" })
+    .setIssuedAt()
+    .setExpirationTime("7d")
+    .sign(encodedKey);
+}
+
+export async function decrypt(session: string | undefined = "") {
+  try {
+    const { payload } = await jwtVerify(session, encodedKey, {
+      algorithms: ["HS256"],
+    });
+    return payload;
+  } catch (error) {
+    console.log("Failed to verify session");
+  }
+}
+
+// Settings cookies
+export async function createSession(userId: string) {
+  const expiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
+  const session = await encrypt({ userId, expiresAt });
+
+  cookies().set("session", session, {
+    httpOnly: true,
+    secure: true,
+    expires: expiresAt,
+    sameSite: "lax",
+    path: "/",
+  });
+}
